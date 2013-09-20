f0ed7f6beda0c62d867c10dd054516222d93f5b6
Morten Siebuhr <sbhr@sbhr.dk>
2013-09-20 10:58:10 +0200

    Hashing: Implement GetNode(key) & GetNodes(key).
    It's not terribly efficient, but it does seem to work OK.

--- Hashing/hashing.go
+++ Hashing/hashing.go
@@ -5,21 +5,39 @@ package Hashing
 // Should behave as https://github.com/graphite-project/carbon/blob/master/lib/carbon/hashing.py
 
 import (
-	"fmt"
 	"crypto/md5"
+	"fmt"
+	"sort"
 )
 
+type ringEntry struct {
+	hash uint16
+	key  string
+}
+
+type ringEntries []*ringEntry
+
+func (s ringEntries) Len() int           { return len(s) }
+func (s ringEntries) Swap(i, j int)      { s[i], s[j] = s[j], s[i] }
+func (s ringEntries) Less(i, j int) bool { return s[i].hash < s[j].hash }
+
 type ConsistentHashRing struct {
-	ring map[uint16]string
-	nodes []string
+	ring         ringEntries
+	nodes        []string
 	replicaCount int
 }
 
 func NewConsistentHashRing(nodes []string, replicaCount int) *ConsistentHashRing {
 	s := &ConsistentHashRing{}
-	s.ring = make(map[uint16]string)
+	s.ring = ringEntries{}
 	s.nodes = make([]string, 0)
 	s.replicaCount = replicaCount
+
+	// Add the given nodes
+	for _, n := range nodes {
+		s.addNode(n)
+	}
+
 	return s
 }
 
@@ -28,7 +46,7 @@ func computeRingPosition(key string) uint16 {
 	h := md5.New()
 	h.Write([]byte(key))
 	sum := h.Sum(nil)
-	i := uint16(sum[0]) << 8 | uint16(sum[1])
+	i := uint16(sum[0])<<8 | uint16(sum[1])
 	return i
 }
 
@@ -37,38 +55,90 @@ func (s *ConsistentHashRing) addNode(node string) {
 	s.nodes = append(s.nodes, node)
 
 	// Insert node into ring
-	for i := 0; i < s.replicaCount ; i += 1 {
+	for i := 0; i < s.replicaCount; i += 1 {
 		key := fmt.Sprintf("%s:%d", node, i)
 		position := computeRingPosition(key)
-		s.ring[position] = node
+		s.ring = append(s.ring, &ringEntry{position, node})
 	}
+	sort.Sort(s.ring)
 }
 
 func (s *ConsistentHashRing) removeNode(node string) {
-	// Remove node from s.nodes
-	newNodes := make([]string, 0)
-	for _, nnode := range s.nodes {
-		if node != nnode {
-			newNodes = append(newNodes, nnode)
+	/*
+		// Remove node from s.nodes
+		newNodes := make([]string, 0)
+		for _, nnode := range s.nodes {
+			if node != nnode {
+				newNodes = append(newNodes, nnode)
+			}
 		}
-	}
-	s.nodes = newNodes
+		s.nodes = newNodes
 
-	// Remove node-references from the ringitself
-	positionsToDelete := make([]uint16, 0)
-	// Remove from ring
-	for rpos, rnode := range s.ring {
-		if (rnode == node) {
-			positionsToDelete = append(positionsToDelete, rpos)
+		// Remove node-references from the ringitself
+		positionsToDelete := make([]uint16, 0)
+		// Remove from ring
+		for rpos, rnode := range s.ring {
+			if rnode == node {
+				positionsToDelete = append(positionsToDelete, rpos)
+			}
 		}
-	}
 
-	for _, pos := range positionsToDelete {
-		delete(s.ring, pos)
-	}
+		for _, pos := range positionsToDelete {
+			delete(s.ring, pos)
+		}
+	*/
+}
+
+func (s *ConsistentHashRing) getFirstMatchingIndex(key string) int {
+	keyHash := computeRingPosition(key)
+
+	// TODO: Calculate offset if we're wrapping
+
+	// Figure out offset
+	nextIndexNoWrap := sort.Search(
+		len(s.ring),
+		func(i int) bool { return s.ring[i].hash >= keyHash },
+	)
+
+	return nextIndexNoWrap
+}
+
+// Return first node hit in the ring for the given key.
+func (s *ConsistentHashRing) GetNode(key string) string {
+	return s.ring[s.getFirstMatchingIndex(key)].key
 }
 
 // Return all relevant nodes to send the given key to.
-//func (s *ConsistentHashRing) GetNodes(key string) []string {
-	//return
-//}
+//
+// Trivial implementation that makes no attempts at being fast...
+func (s *ConsistentHashRing) GetNodes(key string) []string {
+	// Array of keys to fill out
+	out := make([]int, 1)
+
+	// Get the first element
+	out[0] = s.getFirstMatchingIndex(key)
+
+	// Search forward in the hash until we have seen each server once
+	for i := (out[0] + 1) % len(s.ring); i != out[0]; i = (i + 1) % len(s.ring) {
+		// Check if we already have seen this node
+		seen := false
+		for _, o := range out {
+			if s.ring[i].key == s.ring[o].key {
+				seen = true
+			}
+		}
+
+		if !seen {
+			out = append(out, i)
+		}
+	}
+
+	// Convert output to something sensible
+	outStrings := make([]string, len(out))
+	for i, o := range out {
+		outStrings[i] = s.ring[o].key
+	}
+
+	// Can return nodes in any order
+	return outStrings
+}
--- Hashing/hashing_test.go
+++ Hashing/hashing_test.go
@@ -28,3 +28,58 @@ func BenchmarkComputeRingPosition(b *testing.B) {
 		_ = computeRingPosition("a")
 	}
 }
+
+var hashRingOutput_a_b_c_10 = []struct {
+	in  string
+	out []string
+}{
+	{"foo", []string{"b", "a", "c"}},
+	{"bar", []string{"c", "a", "b"}},
+	{"baz", []string{"b", "c", "a"}},
+	{"foobar", []string{"c", "a", "b"}},
+	{"a", []string{"c", "a", "b"}},
+	{"b", []string{"a", "b", "c"}},
+	{"c", []string{"c", "b", "a"}},
+	{"1", []string{"c", "b", "a"}},
+	{"2", []string{"c", "b", "a"}},
+	{"3", []string{"c", "b", "a"}},
+}
+
+func TestBasicHashRing10Replicas(t *testing.T) {
+	// Create new ring
+	r := NewConsistentHashRing([]string{"a", "b", "c"}, 10)
+
+	for _, tt := range hashRingOutput_a_b_c_10 {
+		// GetNode(key) should return the first node it should hit
+		node := r.GetNode(tt.in)
+		if node != tt.out[0] {
+			t.Errorf("Expected GetNode(%v) to return '%v', got '%v'", tt.in, tt.out[0], node)
+		}
+
+		// GetNodes(key) should return all the nodes in order of appearence in the hash
+		nodes := r.GetNodes(tt.in)
+		if len(nodes) != 3 {
+			t.Errorf("Expected GetNodes(%v) to return 3 elements, got %v", tt.in, len(nodes))
+		} else if nodes[0] != tt.out[0] || nodes[1] != tt.out[1] || nodes[2] != tt.out[2] {
+			t.Errorf("Expected GetNodes(%v) to return '%v', got '%v'", tt.in, tt.out, nodes)
+		}
+	}
+}
+
+func BenchmarkGetNode_3_10(b *testing.B) {
+	r := NewConsistentHashRing([]string{"a", "b", "c"}, 10)
+	keys := []string{"foo", "bar", "baz", "1", "2", "3"}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		r.GetNode(keys[i%len(keys)])
+	}
+}
+
+func BenchmarkGetNode_12_100(b *testing.B) {
+	r := NewConsistentHashRing([]string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l"}, 100)
+	keys := []string{"foo", "bar", "baz", "1", "2", "3"}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		r.GetNode(keys[i%len(keys)])
+	}
+}
